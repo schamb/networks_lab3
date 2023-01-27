8f64a08a409bf5936670bb430e6d0df250ed1999
Shayna Chambless <shaychamblessshay@gmail.com>
2023-01-27 08:05:06 -0800

    commit

--- pdu_functions.c
+++ pdu_functions.c
@@ -32,6 +32,10 @@ int sendPDU(int clientSocket, uint8_t* dataBuffer, int lengthOfData){
 int recvPDU(int socketNumber, uint8_t* dataBuffer, int bufferSize){
     uint8_t message_length;
     safeRecv(socketNumber, &message_length, 2, MSG_WAITALL);
+    if(message_length <= 0){
+        fprintf(stderr, "message length");
+        exit(1);
+    }
     int n = safeRecv(socketNumber, dataBuffer, message_length, MSG_WAITALL);
 
     return n;
